6aafdfacb9c65579247e08c6eb9558cf9ae63e62
Magnesium- <757057999@qq.com>
2019-12-26 00:57:37 +0800

    learn bind->lifecycle

---
+++ office-tutorail/bind/b-03.svelte
@@ -0,0 +1,14 @@
+<script>
+  let yes = false;
+</script>
+
+<label>
+  <input type="checkbox" bind:checked={yes} />
+  Yes! Send me regular email span
+</label>
+
+{#if yes}
+  <p>Thank you. We will bombard your inbox and sell your personal details.</p>
+{:else}
+  <p>You must opt in to continue. If you're not paying, you're the product.</p>
+{/if}
---
+++ office-tutorail/lifecycle/l-02.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { onDestroy } from 'svelte';
+
+  let seconds = 0;
+  
+  import { onDestroy } from 'svelte';
+
+export function onInterval(callback, milliseconds) {
+	// implementation goes here
+}
+</script>
+
+<p>
+	The page has been open for
+	{seconds} {seconds === 1 ? 'second' : 'seconds'}
+</p>
+
